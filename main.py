4145a020e2d68e2834f2bb74e8b1f711886a3caf
090631 <92621700+090631@users.noreply.github.com>
2021-10-18 14:07:54 +0600

    Readability changes
    Hey, really loved your video. Thought your code needed a bit of readability changes, hope you don't mind. I recently got the idea to make a dank bot myself. Looks like you beat me to it 😅 I just finished mine. Not as pro as yours but has some added optionality. Made 20-30 mil(If some items are traded) in 3 days with it. It uses a dummy account to farm resources then transfers to my main account. Would love it if you gave your thoughts on it => https://github.com/090631/DankFarmBot

--- main.py
+++ main.py
@@ -1,50 +1,53 @@
-from pynput import keyboard
-from pynput.keyboard import Controller as KeyboardController
-from pynput.keyboard import Key
-import pyautogui
-from discord import utils
-import threading
-import random
-import time
 import os
-FLAG = True
+import time
+import random
+import threading
+import pyautogui as pg
+from discord import utils
+from pynput import keyboard
+from pynput.keyboard import Key
+from pynput.keyboard import Controller as  KeyboardController
 
+current_time = time.strftime("%H:%M:%S", time.localtime())
+
+# ///===========================================Global Functions====================================================///
+
+FLAG = True
 counter = 0
 first = 0
 last = 0
 
+# ///===========================================Global Functions====================================================///
 
-current_time = time.strftime("%H:%M:%S", time.localtime())
-print('-'*33)
-print(f'The time of starting is: {current_time}')
-print('-'*33 + '\n\n')
 
 def send(message):
     keyboard1 = KeyboardController()
-
     keyboard1.type(message)
     keyboard1.press(Key.enter)
     keyboard1.release(Key.enter)
 
+    
 def beg_loop():
     while FLAG:
         send('pls beg')
         send('pls dep all')
-
         time.sleep(random.randint(46, 50))
 
+        
 def findButton():
     color = (88, 101, 242)
     clicked = False
-    s = pyautogui.screenshot()
+    s = pg.screenshot()
+    
     for x in range(s.width):
         for y in range(s.height):
             if s.getpixel((x, y)) == color:
-                pyautogui.click(x, y)
+                pg.click(x, y)
                 clicked = True
                 break 
         if clicked == True:
             break
+            
     keyboard2 = KeyboardController()
     keyboard2.press(Key.tab)
     keyboard2.release(Key.tab)
@@ -64,6 +67,12 @@ def search_loop(counter, first, last):
 
 
 
+# ///==========================================Program Start Point==================================================///
+        
+
+print('-'*33)
+print(f'The time of starting is: {current_time}')
+print('-'*33 + '\n\n')
 threads = [threading.Thread(target=beg_loop), threading.Thread(target=search_loop(counter, first, last))]
 
 for thread in threads:
